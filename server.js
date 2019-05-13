788405d7e62c853e1b63e40a0ad7a1b69a7a3eb3
Simon van Endern <simon.koenigx@gmail.com>
2019-05-13 14:48:14 +0200

    Implemented user path stub

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 8888
 
+function handleNewUserRequest(req, res) {
+	// TODO: Implement!!
+	console.log("Got new user request")
+	res.status(200).send("Ok")
+}
+
 app.get('/', function (req, res) {
 	res.set({
 		'Content-Type': 'text/plain'
@@ -9,6 +15,8 @@ app.get('/', function (req, res) {
 	res.status(200)
 		.send("Hello world")
 })
+
+app.post('/user', handleNewUserRequest)
 
 const server = app.listen(port, function () {
 	console.log("Server listening on port " + port)
