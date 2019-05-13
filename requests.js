3129d81f5702aa3be2abd02c5466deb3fd3c526f
Simon van Endern <simon.koenigx@gmail.com>
2019-05-13 15:10:54 +0200

    Added routes for all needed functionality and implemented stubs for handling those requests

--- requests.js
+++ requests.js
@@ -19,8 +19,44 @@ function handleBasicGetRequest (req, res) {
 		.send("Hello world")
 }
 
+function sendAPIInfo (req, res) {
+	res.set({
+		'Content-Type': 'text/json'
+	})
+	res.status(200).end()
+}
+
+function sendAggregations (req, res) {
+	res.set({
+		'Content-Type': 'text/json'
+	})
+	res.status(200).send("List of all aggregations as json")
+}
+
+function sendStatistics (req, res) {
+	res.set({
+		'Content-Type': 'text/json'
+	})
+	res.status(200).send({'basic':'info'})
+}
+
+function handleAggregationResult (req, res) {
+	console.log("Got aggregation result")
+	res.status(200).end()
+}
+
+function handleForwardRequest (req, res) {
+	console.log("Got forward request")
+	res.status(200).end()
+}
+
 exports.handleNewUserRequest = handleNewUserRequest
 exports.handleUnknownRequest = handleUnknownRequest
 exports.handleBasicGetRequest = handleBasicGetRequest
+exports.sendAPIInfo = sendAPIInfo
+exports.sendAggregations = sendAggregations
+exports.sendStatistics = sendStatistics
+exports.handleAggregationResult = handleAggregationResult
+exports.handleForwardRequest = handleForwardRequest
 
 module.exports = exports
--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const port = 8888
 const RequestHandling = require('./requests')
 
 app.get('/', RequestHandling.handleBasicGetRequest)
+app.get('/info', RequestHandling.sendAPIInfo)
+app.get('/aggregations', RequestHandling.sendAggregations)
+app.get('/stats', RequestHandling.sendStatistics)
 app.post('/user', RequestHandling.handleNewUserRequest)
+app.post('/aggregation', RequestHandling.handleAggregationResult)
+app.post('/forward', RequestHandling.handleForwardRequest)
 app.all('*', RequestHandling.handleUnknownRequest)
 
 const server = app.listen(port, function () {
